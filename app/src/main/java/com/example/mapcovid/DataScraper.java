dc1cd4b28dd77ac0eb851e8f4c23ea2afc68e681
niconeven <nneven@usc.edu>
2021-03-08 22:18:42 -0800

    add new activity to AndroidManifest.xml

--- app/src/main/java/com/example/mapcovid/DataScraper.java
+++ app/src/main/java/com/example/mapcovid/DataScraper.java
@@ -1,5 +1,14 @@
 package com.example.mapcovid;
 
-public class DataScraper {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataScraper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scraper_test);
+    }
 
 }
