b9ff71d35ed0ff3fd9c0eb7718197394be8a7ecc
niconeven <nneven@usc.edu>
2021-03-08 22:40:47 -0800

    add test_view and TestActivity

--- app/src/main/java/com/example/mapcovid/DataScraper.java
+++ app/src/main/java/com/example/mapcovid/DataScraper.java
@@ -1,14 +1,48 @@
 package com.example.mapcovid;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.AsyncTask;
+import java.io.IOException;
 
-public class DataScraper extends AppCompatActivity {
+public class DataScraper extends AsyncTask<Void, Void, Void> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.scraper_test);
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog = new ProgressDialog(TestActivity.this);
+        progressDialog.show();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            //Connect to the website
+            Document document = Jsoup.connect(url).get();
+
+            //Get the logo source of the website
+            Element img = document.select("img").first();
+            // Locate the src attribute
+            String imgSrc = img.absUrl("src");
+            // Download image from URL
+            InputStream input = new java.net.URL(imgSrc).openStream();
+            // Decode Bitmap
+            bitmap = BitmapFactory.decodeStream(input);
+
+            //Get the title of the website
+            title = document.title();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        imageView.setImageBitmap(bitmap);
+        textView.setText(title);
+        progressDialog.dismiss();
     }
 
 }
---
+++ app/src/main/java/com/example/mapcovid/TestActivity.java
@@ -0,0 +1,14 @@
+package com.example.mapcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_view);
+    }
+
+}
