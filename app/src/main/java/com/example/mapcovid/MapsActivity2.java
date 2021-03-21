9065e39303326e999c4791cfff4e409d99ce0cf7
ykelty <ykelty@usc.edu>
2021-03-21 05:16:08 -0700

    Revert "Revert "maps""
    This reverts commit 0374b0901e83406646a57017981f311670859606.

--- app/src/main/java/com/example/mapcovid/MapsActivity2.java
+++ app/src/main/java/com/example/mapcovid/MapsActivity2.java
@@ -224,10 +224,9 @@ public class MapsActivity2 extends AppCompatActivity implements OnMapReadyCallba
         map.addMarker(new MarkerOptions().position(crenshawkiosk).title("Crenshaw Kiosk Testing"));
         LatLng jwch = new LatLng(34.04338,-118.24290);
         map.addMarker(new MarkerOptions().position(jwch).title("JWCH Institute Testing"));
-        LatLng jwch = new LatLng(34.04338,-118.24290);
-        map.addMarker(new MarkerOptions().position(jwch).title("JWCH Institute Testing"));
+
         LatLng community = new LatLng(34.05608,-118.27463);
-        map.addMarker(new MarkerOptions().position(jwch).title("Angeles Community Testing"));
+        map.addMarker(new MarkerOptions().position(community).title("Angeles Community Testing"));
 
     }
 
