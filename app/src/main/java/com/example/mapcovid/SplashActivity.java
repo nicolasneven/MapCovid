a48b406152d7e6835056aac874345ae8b0b43826
Charles Oxyer <oxyer@usc.edu>
2021-03-21 22:37:49 -0400

    first time app opening

--- app/src/main/java/com/example/mapcovid/SplashActivity.java
+++ app/src/main/java/com/example/mapcovid/SplashActivity.java
@@ -7,22 +7,45 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.content.SharedPreferences;
+import android.util.*;
 
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        SharedPreferences pref = SplashActivity.this.getSharedPreferences("pref",0);
+        SharedPreferences.Editor editor= pref.edit();
+        boolean firstRun = pref.getBoolean("firstRun", true);
+        if(firstRun)
+        {
+            Log.i("onCreate: ","first time" );
+            editor.putBoolean("firstRun",false);
+            editor.commit();
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    Intent homeIntent = new Intent(SplashActivity.this, Onboarding1.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
+        }
+        else
+        {
+            Log.i("onCreate: ","second time");
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    Intent homeIntent = new Intent(SplashActivity.this, MapsActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            }, SPLASH_TIME_OUT);
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                Intent homeIntent = new Intent(SplashActivity.this, Onboarding1.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        }
+        setContentView(R.layout.splash_screen);
 
     }
 }
