ff5ac98ffa9094e3a3597bf6d97ab1e128971435
Louis Chen <chenloui@usc.edu>
2021-03-21 21:58:21 -0400

    onboarding files

--- app/src/main/java/com/example/mapcovid/Onboarding4.java
+++ app/src/main/java/com/example/mapcovid/Onboarding4.java
@@ -1,16 +1,13 @@
 package com.example.mapcovid;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
-public class Onboarding1 extends AppCompatActivity{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Onboarding4 extends AppCompatActivity{
 
     Button next;
 
@@ -26,7 +23,7 @@ public class Onboarding1 extends AppCompatActivity{
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button3:
-                startActivity(new Intent(getApplicationContext(), Onboarding2.class));
+                startActivity(new Intent(getApplicationContext(), Onboarding5.class));
                 overridePendingTransition(0, 0);
         }
     }
