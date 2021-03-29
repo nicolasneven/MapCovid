c6115181bd81e6df011682d6a225e585646bcf62
Charles Oxyer <oxyer@usc.edu>
2021-03-28 20:23:12 -0700

    fixed dependency

---
+++ app/src/test/java/com/example/mapcovid/OnboardingTest.java
@@ -0,0 +1,23 @@
+package com.example.mapcovid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import android.view.View;
+import android.widget.Button;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import android.view.View;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.mock;
+
+
+public class OnboardingTest {
+    private Onboarding1 oboard;
+    @Test
+    public void onboarding1OnClickListener(){
+        assertEquals(4, 2 + 2);
+
+    }
+}
