ea4490a6f0c056dc3d8d33921dac53cfe2ddbff2
Louis Chen <chenloui@usc.edu>
2021-03-29 00:20:12 -0400

    Create aboutTest.java

---
+++ app/src/androidTest/java/com/example/mapcovid/aboutTest.java
@@ -0,0 +1,25 @@
+package com.example.mapcovid;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+public class aboutTest {
+
+    //make sure about tab is displayed when the about button is clicked
+    @Test
+    public void greeterSaysHello() {
+
+        // click the about button
+        onView(withId(R.id.viewmore)).perform(click());
+
+
+        // check that the text is displayed
+        onView(withText("Hello Steve!")).check(matches(isDisplayed()));
+    }
+
+}
