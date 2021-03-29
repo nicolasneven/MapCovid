6f2dd9756fe06ac1c407ff39ec29b642e3cfe98a
Jason Kohan <jasonkoh@usc.edu>
2021-03-28 17:55:37 -0700

    dependecies

---
+++ app/src/androidTest/java/com/example/mapcovid/regTest.java
@@ -0,0 +1,27 @@
+package com.example.mapcovid;
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+@RunWith(JUnit4.class)
+
+public class regTest{
+
+    @Rule
+    public ActivityScenarioRule rule =
+            new ActivityScenarioRule<>(MapsActivity.class);
+
+
+    @Test
+    public void myTest() {
+        ActivityScenario scenario = rule.getScenario();
+        // Your test code goes here.
+    }
+
+}
---
+++ app/src/test/java/com/example/mapcovid/SplashActivityTest.java
@@ -0,0 +1,9 @@
+package com.example.mapcovid;
+
+import junit.framework.TestCase;
+
+public class SplashActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+}
