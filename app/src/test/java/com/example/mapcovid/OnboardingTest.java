f4dbff65e719cdf107a1f9f23eabf8c54ebd5e3c
Charles Oxyer <oxyer@usc.edu>
2021-03-29 10:22:42 -0700

    onboarding whitebox test

--- app/src/test/java/com/example/mapcovid/OnboardingTest.java
+++ app/src/test/java/com/example/mapcovid/OnboardingTest.java
@@ -3,6 +3,9 @@ package com.example.mapcovid;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import org.mockito.Mock;
@@ -11,13 +14,20 @@ import android.view.View;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class OnboardingTest {
-    private Onboarding1 oboard;
+    @Mock
+    Onboarding1 oboard;
+    //Checks if Button is successfully created in onboarding screens
     @Test
-    public void onboarding1OnClickListener(){
-        assertEquals(4, 2 + 2);
+    public void onboardingButtonTest(){
+        oboard = new Onboarding1();
+        Button b;
+        b = oboard.createButton();
+        assertNotNull(b);
 
     }
 }
