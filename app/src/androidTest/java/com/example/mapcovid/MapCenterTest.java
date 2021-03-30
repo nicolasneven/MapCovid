0c56c604c7cf70368109a64761ed6582bfad56e7
Louis Chen <chenloui@usc.edu>
2021-03-29 23:44:42 -0400

    settings and map blackbox tests

---
+++ app/src/androidTest/java/com/example/mapcovid/MapCenterTest.java
@@ -0,0 +1,37 @@
+package com.example.mapcovid;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MapCenterTest {
+    @Rule public ActivityScenarioRule<MapsActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MapsActivity.class);
+
+    @Test
+    public void loadMap() throws InterruptedException {
+        for (int i = 0; i < 20; i++) {
+            onView(withId(R.id.map)).perform();
+            Thread.sleep(10000);
+            System.out.println(i);
+        }
+    }
+    //Manually click on the blue region
+    //A pop-up should appear on the bottom with the following text
+    //University Park
+    //Cases: 3395
+    //Deaths: 51
+}
+
