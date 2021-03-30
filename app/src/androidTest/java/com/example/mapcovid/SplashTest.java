2be86f85ab9150ed9b88ed6b065b421035624119
Jason Kohan <jasonkoh@usc.edu>
2021-03-29 21:26:22 -0700

    testing

--- app/src/androidTest/java/com/example/mapcovid/SplashTest.java
+++ app/src/androidTest/java/com/example/mapcovid/SplashTest.java
@@ -4,29 +4,24 @@ package com.example.mapcovid;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-
-public class regTest{
-
+public class SplashTest {
     @Rule
     public ActivityScenarioRule<SplashActivity> activityScenarioRule
             = new ActivityScenarioRule<>(SplashActivity.class);
 
 
     @Test
-    public void mapScreenWithoutRegistration() throws InterruptedException {
-        onView(withId(R.layout.splash_screen));
-        Thread.sleep(5000);
+    public void correctImageTest() throws InterruptedException {
+        onView(withId(R.id.imageView2)).check(matches(isDisplayed()));
     }
-
 }
