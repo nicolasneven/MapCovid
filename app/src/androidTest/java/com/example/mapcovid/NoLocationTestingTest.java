c8d7bbf5ef520a4b23c98c1d45ba8b771a603147
ykelty <ykelty@usc.edu>
2021-03-29 21:33:38 -0700

    No Location Testing Center

--- app/src/androidTest/java/com/example/mapcovid/NoLocationTestingTest.java
+++ app/src/androidTest/java/com/example/mapcovid/NoLocationTestingTest.java
@@ -16,18 +16,19 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class TestingCenterTest {
+public class NoLocationTestingTest {
     @Rule public ActivityScenarioRule<MapsActivity> activityScenarioRule
             = new ActivityScenarioRule<>(MapsActivity.class);
 
     @Test
-    public void ViewPopUpPin() throws InterruptedException {
+    public void ViewPopUpPinWithoutLocation() throws InterruptedException {
 
         onView(withId(R.id.map)).perform();
-        Thread.sleep(20000);
+        Thread.sleep(40000);
 
     }
-    //If a location permissions popup appears, then grant permission
+    //Before running, manually go to settings and do not allow location
+    //Then run the map
     // Manually move map to see a Testing Location pin
     //Manually click on the pin
     //A pop up should appear from the pin that shows the Testing Center name and its hours
