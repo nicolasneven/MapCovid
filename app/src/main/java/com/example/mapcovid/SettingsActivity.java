b50d9d30ddb4c4e2e77906a069547afae34b495c
Louis Chen <chenloui@usc.edu>
2021-04-11 15:48:09 -0400

    test notification button

--- app/src/main/java/com/example/mapcovid/SettingsActivity.java
+++ app/src/main/java/com/example/mapcovid/SettingsActivity.java
@@ -2,14 +2,22 @@ package com.example.mapcovid;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.animation.Animator;
 import android.animation.ValueAnimator;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -32,7 +40,7 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
     ValueAnimator mAnimator;
     private Button toggleDark;
 
-    /*
+    /* Following code was used for white box tests
     Button location;
     Button notification;
     Button about;
@@ -76,6 +84,7 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createNotificationChannel("234");
         setContentView(R.layout.settings_activity);
 
         BottomNavigationView navView = findViewById(R.id.bottom_navigation);
@@ -145,6 +154,67 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
             public void onClick(View v) {
                 deleteCache(context);
                 Toast.makeText(getApplicationContext(),"Data has been cleared!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //test notification button
+        Button b1=(Button)findViewById(R.id.notification);
+        String CHANNEL_ID = "234";
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String tittle= "MapCovid Notification Test";
+                //String subject= "Notification Button";
+                //String body= "Button Pressed";
+
+                // Create an explicit intent for an Activity in your app
+                //Intent intent = new Intent(this, AlertDetails.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, new Intent(), 0);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.notification)
+                        .setContentTitle("MapCovid Test Notification")
+                        .setContentText("Test Notification Button was pressed!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        // Set the intent that will fire when the user taps the notification
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+                int notificationId = 0;
+                // notificationId is a unique int for each notification that you must define
+                notificationManager.notify(notificationId, builder.build());
+
+                /*
+                int NOTIFICATION_ID = 234;
+                String CHANNEL_ID = "my_channel_01";
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    CharSequence name = "my_channel";
+                    String Description = "This is my channel";
+                    int importance = NotificationManager.IMPORTANCE_HIGH;
+                    NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                    mChannel.setDescription(Description);
+                    mChannel.enableLights(true);
+                    mChannel.setLightColor(Color.RED);
+                    mChannel.enableVibration(true);
+                    mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                    mChannel.setShowBadge(false);
+                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(mChannel);
+                }
+
+                NotificationManager notif=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                Notification notify=new Notification.Builder
+                        (getApplicationContext()).setContentTitle(tittle).setContentText(body).
+                        setContentTitle(subject).setSmallIcon(R.drawable.notification).build();
+
+                int NOTIFICATION_ID = 234;
+
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                notif.notify(NOTIFICATION_ID, notify);
+                 */
             }
         });
 
@@ -250,5 +320,24 @@ public class SettingsActivity extends AppCompatActivity implements OnClickListen
 
         }
     }
+
+    private void createNotificationChannel(String channel_id) {
+
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notification Channel";
+            String description = "Channel for Notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            //String channel_id = "234";
+            NotificationChannel channel = new NotificationChannel(channel_id, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
 }
