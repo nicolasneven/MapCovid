322f504192437b4cfca6604dedbd3206901a2de8
Louis Chen <chenloui@usc.edu>
2021-04-30 01:38:18 -0400

    Update CalendarActivity.java

--- app/src/main/java/com/example/mapcovid/CalendarActivity.java
+++ app/src/main/java/com/example/mapcovid/CalendarActivity.java
@@ -1,11 +1,23 @@
 package com.example.mapcovid;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -33,4 +45,31 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
     }
+
+    Context context = getApplicationContext();
+
+    //get JSON and parse it
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    protected void getHistory() throws FileNotFoundException {
+        FileInputStream fis = context.openFileInput("locations");
+        InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(inputStreamReader)){
+            String line = reader.readLine();
+            while(line != null){
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e){
+            // Error occurred when opening raw file for reading
+        } finally {
+            String contents = stringBuilder.toString();
+        }
+    }
+
+    public static Date unix_to_date(long timeStamp){
+        java.util.Date time = new java.util.Date((long)timeStamp*1000);
+        return time;
+    }
+
 }
